8b5a40ce29051d7d7cfee98f335577c3c2bf9164
Akhilsoe <19cs011akhi@ug.cusat.ac.in>
2022-09-05 21:34:39 +0530

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const handleClick = () => setnav(!nav);
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed w-full h-[40px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div></div>
 
       {/* Menu */}
